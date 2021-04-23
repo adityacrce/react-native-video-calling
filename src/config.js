eaec918205d822d9aa56dd87c170c596020605b7
adityacrce <adi.crce@gmail.com>
2021-04-23 21:27:08 +0530

    final commit

---
+++ src/config.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    appId: 3657,
+    authKey: '5T2OpMOtrzGP-gY',
+    authSecret: 'jVSXCgRshDsqB3p',
+  },
+  {
+    debug: {mode: 1},
+  },
+];
+
+export const users = [
+  {
+    id: 2617064,
+    name: 'aditya',
+    login: 'aditya',
+    password: 'Connectycube',
+    color: '#34ad86',
+  },
+  {
+    id: 2617971,
+    name: 'abhishek',
+    login: 'abhishek',
+    password: 'Connectycube1',
+    color: '#056a96',
+  },
+  {
+    id: 2628610,
+    name: 'albin',
+    login: 'albin',
+    password: 'Connectycube2',
+    color: '#077988',
+  },
+];
